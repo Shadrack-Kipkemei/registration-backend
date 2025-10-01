0e37dbdb8dc9a9b6b822d6e88f7fc8781b9fb3b9
Shadrack <shadrackkemei2019@gmail.com>
2025-10-01 16:48:38 +0300

    Initialize Flask application with CORS and register blueprints

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask 
+from flask_cors import CORS 
+from routes.meeting import meeting_bp
+from routes.registration import registration_bp
+from routes.admin import admin_bp
+from routes.locations import locations_bp
+
+
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+
+# Register blueprints
+app.register_blueprint(meeting_bp, url_prefix='/api')
+app.register_blueprint(registration_bp, url_prefix='/api')
+app.register_blueprint(admin_bp, url_prefix='/api/admin')
+app.register_blueprint(locations_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
